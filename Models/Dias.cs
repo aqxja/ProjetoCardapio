034b98893ccce118c284c85cbd132dceef998d9a
Kaique <kaique.procopio@aluno.senai.br>
2023-10-26 13:20:41 -0300

    ALteração Dias

--- Models/Dias.cs
+++ Models/Dias.cs
@@ -1,10 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjetoCardapio.Models
 {
     [Table("Dias")]
     public class Dias
     {
-        [Column]
+        [Column("DiaId")]
+        [Display(Name = "Id dia")]
+
+        public int id { get; set; }
+
+        [Column("DiaNome")]
+        [Display(Name = "Nome dia")]
+
+        public string NomeDia { get; set; } = string.Empty;
     }
 }
