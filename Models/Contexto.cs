ec58f1828ae2cbeeaf3a36c15fe20f8b617e7d07
aqxja <aquiles123aq@gmail.com>
2023-10-26 08:33:26 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoCardapio.Models
+{
+
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+    }
+}
