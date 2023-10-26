821f2d356d314e4a47197d8c698d16114c836ac1
Alue01 <alueandreoli@gmail.com>
2023-10-26 11:49:58 -0300

    salas começo

--- Models/Salas.cs
+++ Models/Salas.cs
@@ -1,9 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjetoCardapio.Models
 {
     [Table("Salas")]
     public class Salas
     {
+        [Column("SalasId")]
+        [Display(Name = "Id das Salas")]
+
+        public int id { get; set; }
+
+        [Column("SalasNome")]
+        [Display(Name = "Nome das Salas")]
+
+        public string NomeSalas { get; set; } = string.Empty;
     }
 }
